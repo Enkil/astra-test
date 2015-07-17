bfb7d474bdc093ea00267e3283847592d947f523
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-17 11:21:28 +0300

    index page 70%

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -4,10 +4,4 @@
 
 /* Third party */
 
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/bootstrap/dist/js/bootstrap.js
-//= ../../bower_components/bootstrap-material-design/dist/js/ripples.js
-//= ../../bower_components/bootstrap-material-design/dist/js/material.js
-//= ../../bower_components/modernizr/modernizr.js
-//= ../../bower_components/social-likes/src/social-likes.js
 
