04a3204debe54b5f754b13c1b6f0d28b6d9592c1
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-17 14:55:40 +0300

    Update 2015-07-17T11:55:39.262Z

--- js/main.js
+++ js/main.js
@@ -9352,18 +9352,18 @@ $(document).ready(function() {
     function redirectPage() {
         window.location = linkLocation;
     }
-    //function getBgColorHex(elem){
-    //    var color = elem.css('background-color')
-    //    var hex;
-    //    if(color.indexOf('#')>-1){
-    //        //for IE
-    //        hex = color;
-    //    } else {
-    //        var rgb = color.match(/\d+/g);
-    //        hex = '#'+ ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2);
-    //    }
-    //    return hex;
-    //}
+    function getBgColorHex(elem){
+        var color = elem.css('background-color')
+        var hex;
+        if(color.indexOf('#')>-1){
+            //for IE
+            hex = color;
+        } else {
+            var rgb = color.match(/\d+/g);
+            hex = '#'+ ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2);
+        }
+        return hex;
+    }
     
     //console.info(colorCookie);
     
