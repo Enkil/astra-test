15e5125f99e854429905f622fba2fffabd7a19b8
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-17 14:53:14 +0300

    Update 2015-07-17T11:53:13.545Z

--- js/main.js
+++ js/main.js
@@ -9352,20 +9352,22 @@ $(document).ready(function() {
     function redirectPage() {
         window.location = linkLocation;
     }
-    function getBgColorHex(elem){
-        var color = elem.css('background-color')
-        var hex;
-        if(color.indexOf('#')>-1){
-            //for IE
-            hex = color;
-        } else {
-            var rgb = color.match(/\d+/g);
-            hex = '#'+ ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2);
-        }
-        return hex;
-    }
+    //function getBgColorHex(elem){
+    //    var color = elem.css('background-color')
+    //    var hex;
+    //    if(color.indexOf('#')>-1){
+    //        //for IE
+    //        hex = color;
+    //    } else {
+    //        var rgb = color.match(/\d+/g);
+    //        hex = '#'+ ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2);
+    //    }
+    //    return hex;
+    //}
     
-    console.info(colorCookie);
+    //console.info(colorCookie);
+    
+    colorCurrent = getBgColorHex($('.card__header, .footer')); // current color
     
     colorCookie = $.cookie('colorCookie');
     if (colorCookie != 'undefined') {
@@ -9375,7 +9377,7 @@ $(document).ready(function() {
     $('.footer__logo').mouseenter(function() {
         colors = ['#1a48a6','#a71b3b','#a6781a','#1ba768']; // colors to set
     
-        //colorCurrent = getBgColorHex($('.card__header, .footer')); // current color
+        colorCurrent = getBgColorHex($('.card__header, .footer')); // current color
     
         colorsAmt = colors.length;
         randomColor = Math.floor(Math.random() * colorsAmt);
