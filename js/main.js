a8aa7d99d740043e726c229fe0a40234368de79e
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-17 16:18:13 +0300

    Update 2015-07-17T13:18:10.965Z

--- js/main.js
+++ js/main.js
@@ -9343,32 +9343,28 @@ $(document).ready(function() {
         linkLocation = $(this).data("page") + ".html";
     
         history.pushState(null, null, linkLocation);
-        //$(window).load(linkLocation);
-        $.ajax({
-            url: linkLocation + '?ajax=1',
-            success: function(html){
-                $("html").html(html);
-            }
-        });
-    
-        window.onpopstate = function(event) {
-            if (event && event.state) {
-                location.reload();
-            }
-            ;
-        };
+        $("html").load(linkLocation);
+        //$.ajax({
+        //    url: linkLocation + '?ajax=1',
+        //    success: function(html){
+        //        $("html").html(html);
+        //    }
+        //});
+        //
+        //window.onpopstate = function(event) {
+        //    if (event && event.state) {
+        //        location.reload();
+        //    }
+        //};
     
     
         return false;
+    
     });
     
-    function loadPage(elem) {
-    
-    }
-    
-    //window.addEventListener("popstate", function() {
-    //    loadPage(location.pathname);
-    //});
+    window.onpopstate = function() {
+        $("html").load(linkLocation)
+    };
     function getBgColorHex(elem){
         var color = elem.css('background-color')
         var hex;
